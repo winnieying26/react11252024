f2fe807cc674930a3b3908571d250f5f84fdf1a5
Felix Chen <felixchen3674@gmail.com>
2024-12-06 21:40:31 -0500

    homework solution

--- js-homework/day8_promise/promise.js
+++ js-homework/day8_promise/promise.js
@@ -1,17 +1,25 @@
 export const fetchPosts = async () => {
   //fetch posts from "https://jsonplaceholder.typicode.com/posts"
   // return the posts
-};
 
-// fetch("https://jsonplaceholder.typicode.com/posts")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log("data", data);
-//   });
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+  return posts;
+
+  // return fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+  //   res.json()
+  // );
+};
 
 export const fetchPostById = async (id) => {
   //fetch a post by id from "https://jsonplaceholder.typicode.com/posts/${id}"
   //return the post
+
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+  const post = await response.json();
+  return post;
 };
 
 export const sequentialPromise = async (promises, order) => {
@@ -22,4 +30,12 @@ export const sequentialPromise = async (promises, order) => {
   //order = [2,1,3]
   //promises = ["data1", "data2", "data3"]
   //results = ["data2", "data1", "data3"]
+
+  const result = [];
+  for (const num of order) {
+    console.log(num);
+    const data = await promises[num - 1];
+    result.push(data);
+  }
+  return result;
 };
